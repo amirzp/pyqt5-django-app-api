28ff468647eaa8964063d0491a4232d719c56ccf
amir <amirzabihpour@gmail.com>
2021-01-23 14:53:19 +0330

    edit get request for admin active

--- django_project/contact/models.py
+++ django_project/contact/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 
 class Contact(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='ﮐﺎﺭﺑﺮ')
     name = models.CharField(max_length=20, verbose_name='ﻧﺎﻡ')
     family = models.CharField(max_length=20, verbose_name='ﻧﺎﻡ ﺧﺎﻧﻮاﺩﮔﯽ')
     phone = models.CharField(max_length=20, verbose_name='ﺗﻠﻔﻦ')
--- django_project/contact/views.py
+++ django_project/contact/views.py
@@ -7,3 +7,6 @@ class ContactView(viewsets.ModelViewSet):
     queryset = Contact.objects.all()
     serializer_class = ContactSerializers
     permission_classes = [permissions.IsAdminUser]
+
+    def get_queryset(self):
+        return Contact.objects.filter(user=self.request.user)
