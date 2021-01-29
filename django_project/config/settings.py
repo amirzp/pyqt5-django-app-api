72fe39e4ad117bf77d3737351de4aa0e9275480c
amir <amirzabihpour@gmail.com>
2021-01-29 19:44:27 +0330

    complete refresh token

--- django_project/config/settings.py
+++ django_project/config/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import datetime
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -125,4 +126,8 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework_simplejwt.authentication.JWTAuthentication',
     ),
+}
+
+SIMPLE_JWT = {
+    'ACCESS_TOKEN_LIFETIME': datetime.timedelta(minutes=5),
 }
--- pyqt_project/request_api.py
+++ pyqt_project/request_api.py
@@ -8,25 +8,28 @@ class ApiRequest:
         self.__header = None
 
     def get(self, url: str):
-        request = self.request_api(act='get', url=url, header=self.__header)
+        request = self.request_api(act='get', url=url)
         if request.status_code == 200:
             return True, request.json()
+        # elif request.status_code == 401:
+        #     self.refresh_token()
+        #     return self.get(url=url)
         else:
             return False, request.json()
 
     def delete(self, url: str):
-        request = self.request_api(act='delete', url=url, header=self.__header)
+        request = self.request_api(act='delete', url=url)
         return request.json()
 
     def post(self, url: str, data: dict):
-        request = self.request_api(act='post', url=url, data=data, header=self.__header)
+        request = self.request_api(act='post', url=url, data=data)
         if request.status_code == 201:
             return True
         else:
             return request.json()
 
     def put(self, url: str, data: dict):
-        request = self.request_api(act='put', url=url, data=data, header=self.__header)
+        request = self.request_api(act='put', url=url, data=data)
         if request.status_code == 200:
             return True
         else:
@@ -51,20 +54,25 @@ class ApiRequest:
             return request.json()
 
     def refresh_token(self):
-        pass
+        url = 'http://localhost:8000/contact/v1/token/refresh/'
+        data = {
+            "refresh": f"{self.__refresh}"
+        }
+        request = self.request_api(act='post', url=url, data=data)
+        if request.status_code == 200:
+            request_json = request.json()
+            self.__token = request_json['access']
+            self.__header = {'Authorization': f'Bearer {self.__token}'}
+            print('Access token is refreshed.')  # TODO delete this line code
 
-    @staticmethod
-    def request_api(act: str, url: str, data: dict = None, header: dict = None):
+    def request_api(self, act: str, url: str, data: dict = None):
         try:
             if act == 'get':
-                request = requests.get(headers=header, url=url)
-                return request
+                request = requests.get(headers=self.__header, url=url)
             elif act == 'post':
-                request = requests.post(headers=header, json=data, url=url)
-                return request
+                request = requests.post(headers=self.__header, json=data, url=url)
             elif act == 'put':
-                request = requests.put(headers=header, json=data, url=url)
-                return request
+                request = requests.put(headers=self.__header, json=data, url=url)
             elif act == 'delete':
                 class Request:
                     def __init__(self):
@@ -75,14 +83,31 @@ class ApiRequest:
                         return {'detail': f'{self.detail}'}
 
                 request_class = Request()
-                request = requests.delete(headers=header, url=url)
+                request = requests.delete(headers=self.__header, url=url)
+
                 if request.status_code == 204:
                     request_class.status_code = 200
                     request_class.detail = 'Item is deleted.'
+                elif request.status_code == 401:
+                    flag = self.request_test(request)
+                    if flag is True:
+                        self.refresh_token()
+                        return self.request_api(act=act, url=url, data=data)
+                    else:
+                        request_class.status_code = -1
+                        request_class.detail = 'Item is not deleted. something wrong!'
                 else:
                     request_class.status_code = -1
                     request_class.detail = 'Item is not deleted. something wrong!'
+
                 return request_class
+
+            flag = self.request_test(request)
+            if flag is True:
+                self.refresh_token()
+                return self.request_api(act=act, url=url, data=data)
+            else:
+                return request
 
         except requests.ConnectionError:
             class Request:
@@ -93,3 +118,18 @@ class ApiRequest:
                 def json():
                     return {'detail': 'server is down.'}
             return Request()
+
+    @staticmethod
+    def request_test(data):
+        request = data.json()
+        flag = False
+        for i in request:
+            if i == 'detail':
+                flag = True
+                break
+        if flag is True:
+            txt = request['detail'] == "Given token not valid for any token type"
+            if data.status_code == 401 and txt:
+                return True
+            else:
+                return False
